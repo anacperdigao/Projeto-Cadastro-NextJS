a8032097348cfe847a098e07cbb33593bbd14268
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-18 13:15:13 -0300

    style: table style

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -9,9 +9,9 @@ const Tabela = (props: TabelaProps) => {
     function renderizarCabecalho() {
         return(
             <tr>
-                <th>Código</th>
-                <th>Nome</th>
-                <th>Idade</th>
+                <th className="text-left p-4">Código</th>
+                <th className="text-left p-4">Nome</th>
+                <th className="text-left p-4">Idade</th>
             </tr>
         )
     }
@@ -22,10 +22,10 @@ const Tabela = (props: TabelaProps) => {
         return(
             props.clientes?.map((cliente, index) => {
                 return(
-                    <tr key={cliente.Id}>
-                        <td>{cliente.Id}</td>
-                        <td>{cliente.Nome}</td>
-                        <td>{cliente.Idade}</td>
+                    <tr key={cliente.Id} className={`${index % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}>
+                        <td className="text-left p-4">{cliente.Id}</td>
+                        <td className="text-left p-4">{cliente.Nome}</td>
+                        <td className="text-left p-4">{cliente.Idade}</td>
                     </tr>
                 )
             })
@@ -34,8 +34,11 @@ const Tabela = (props: TabelaProps) => {
 
 
     return (
-        <table>
-            <thead>
+        <table className="w-full rounded-xl overflow-hidden">
+            <thead className={`
+                text-gray-100 
+                bg-gradient-to-r from-purple-500 to-purple-800`
+            }>
                 {renderizarCabecalho()}
             </thead>
             <tbody>
